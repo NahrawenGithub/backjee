e8a6d38855748a73ae4c44511f2f9b3858ce8eac
NahrawenGithub <nahrawen.bh@gmail.ciom>
2021-06-26 13:02:48 +0100

    v2

--- src/main/java/com/example/back/demo/dao/selleraccount/sql/SellerInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.back.demo.dao.selleraccount.sql;
-
-import com.example.back.demo.entity.selleraccount.sql.SellerInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SellerInfoRepository extends JpaRepository<SellerInfo, Integer> {
-}
